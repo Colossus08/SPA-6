95dd88cf0de21273b5863ecfc3ad93710fc5baa8
Aaron Thomas <message4aaron@gmail.com>
2023-02-01 02:59:55 +0530

    have connected script to a local sql database. get command returns the entire database for now.

---
+++ main.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from pydantic import BaseModel
+from fastapi import FastAPI
+import os
+
+database_password=os.environ['Password']
+
+app = FastAPI()
+
+# Setup database connection
+cnx = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=database_password,
+    database="api"
+)
+
+class Item(BaseModel):
+    time: str
+    monday:str
+    tuesday:str
+
+@app.get("/")
+def read_root():
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM timetable")
+    result = cursor.fetchall()
+    return result
